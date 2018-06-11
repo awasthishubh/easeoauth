8292017e487163b0abdf0f04d87559b2c3284a11
Shubham Awasthi <shubham0522@gmail.com>
2018-06-11 17:41:57 +0530

    Throwing error on incomplete call

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
+var serv, _keys = {},
+    _urls = {}
 
-function services() {
-
+function service(service) {
+    serv = service
 }
 
-function urls() {
-
+function urls(initialize,callback, post, redirect) {
+    _urls = {
+        callback,
+        post,
+        redirect,
+        initialize
+    }
 }
 
-function keys() {
-
+function keys(client_id, client_secret) {
+    _keys={client_id, client_secret}
 }
 
-function initialize() {
-
+function exec(app) {
+    if(!(serv))
+        throw Error['err']="Service not configured";
+    if(!(_keys.client_id || _keys.client_secret))
+        throw Error['err']="All keys not provided";
+    if(!(_urls.initialize, _urls.callback, _urls.post))
+        throw Error['err']="All urls not provided";
+    console.log("running");
 }
 
-module.exports={
-    services,
+
+module.exports = {
+    service,
     urls,
     keys,
-    initialize
+    exec
 };

5a90caac3151f7329031d40015cb31722e38ae6d
Shubham Awasthi <shubham0522@gmail.com>
2018-06-11 15:51:01 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function services() {
+
+}
+
+function urls() {
+
+}
+
+function keys() {
+
+}
+
+function initialize() {
+
+}
+
+module.exports={
+    services,
+    urls,
+    keys,
+    initialize
+};

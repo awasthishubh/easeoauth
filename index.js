5bfe7ceb6de224bb3b2ebb30b9a37315e4ed1613
Shubham Awasthi <shubham0522@gmail.com>
2018-06-14 13:05:12 +0530

    Throws error on invalid configurations

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-function exec(config, app, callback) {
-    if (!(config.service))
-        return callback("Service not configured");
-    if (!(config.keys.client_id || config.keys.client_secret))
-        return callback("Keys not provided properly");
-    if (!(config.urls.initialize, config.urls.callback))
-        return callback("Urls not provided properly");
-    config.urls.initialize.replace("\\", "/");
-    config.urls.callback.replace("\\", "/")
+function exec(app, config, callback) {
+    try{
+        if (!(config.service))
+            throw Error("Service not configured");
+        if (!(config.keys.client_id || config.keys.client_secret))
+            throw Error("Keys not provided properly");
+        if (!(config.urls.initialize, config.urls.callback))
+            throw Error("Urls not provided properly");
+        config.urls.initialize.replace("\\", "/");
+        config.urls.callback.replace("\\", "/")
 
-    if (config.urls.initialize[0] === '.') config.urls.callback = config.urls.initialize.slice(1)
-    if (config.urls.callback[0] == '.') config.urls.callback = config.urls.callback.slice(1)
-    if (config.urls.initialize[0] != '/')
-        config.urls.initialize = '/' + config.urls.initialize
-    if (config.urls.callback[0] != '/')
-        config.urls.callback = '/' + config.urls.callback
-    config.service = config.service.toLowerCase()
+        if (config.urls.initialize[0] === '.') config.urls.callback = config.urls.initialize.slice(1)
+        if (config.urls.callback[0] == '.') config.urls.callback = config.urls.callback.slice(1)
+        if (config.urls.initialize[0] != '/')
+            config.urls.initialize = '/' + config.urls.initialize
+        if (config.urls.callback[0] != '/')
+            config.urls.callback = '/' + config.urls.callback
+        config.service = config.service.toLowerCase()
 
-    supported = ["facebook", "instagram", "twitter", "google", "microsoft"]
+        supported = ["facebook", "instagram", "twitter", "google", "microsoft"]
 
-    if (supported.indexOf(config.service) == -1)
-        return callback("Service not supported");
+        if (supported.indexOf(config.service) == -1)
+            throw Error("Service not supported");
 
-    require('./services/' + config.service).func(app, config.keys, config.urls, callback);
+        require('./services/' + config.service).func(app, config.keys, config.urls, callback);
+    } catch(e){
+        return callback(e)
+    }
 }
 
 
